515174bf98156a9aacb2a729aee7a3d689a1d1c4
iliAsghar <asghar.ali.z.a.r.e@gmail.com>
2024-12-14 22:47:27 +0330

    update

--- Library/Controllers/AccountController.cs
+++ Library/Controllers/AccountController.cs
@@ -70,7 +70,7 @@ namespace Library.Controllers
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, user.Email),
-                new Claim("Role", user.Role)
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
             var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
