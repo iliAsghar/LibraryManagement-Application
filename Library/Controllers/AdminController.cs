a815861765f4fa576f386a36178318cc4973af28
bardia <nmty9198@gmail.com>
2024-12-22 12:51:02 -0800

    added admin controller

---
+++ Library/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        private readonly MyDBContext _context;
+
+        public DashboardController(MyDBContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // تعداد کاربران
+            var userCount = _context.Users.Count();
+
+            // تعداد کتاب‌ها
+            var bookCount = _context.Books.Count();
+
+            // تعداد تراکنش‌ها (بدون تراکنش‌های UnFinalized)
+            var transactionCount = _context.Transactions
+                                           .Count(t => t.Status != TransactionStatus.UnFinalized);
+
+            // ارسال اطلاعات به View
+            ViewBag.UserCount = userCount;
+            ViewBag.BookCount = bookCount;
+            ViewBag.TransactionCount = transactionCount;
+
+            return View();
+        }
+    }
+}
